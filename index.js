956a052e883f0b430785cadede2bbdd9a52fa341
thucvn <thucvn@vnpt.vn>
2024-03-05 11:25:35 +0700

    * update count empty

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function isEmpty(data) {
-  return data === undefined || data === null || data === '';
-}
+var _ = require('lodash');
+module.exports = {
+	isEmpty: function(data) {
+	  return data === undefined || data === null || data === '';
+	},
 
-module.exports = isEmpty;
+	countEmpty: function(data) {
+		if (!Array.isArray(data)) return 0;
+		let count = 0;
+		for (let i = 0; i < data.length; i++)
+			if (_.isEmpty(data[i])) count++;
+		return count;
+	}
+}
